367c3e4575e269b4374047eb18ef8199aea512d5
junko911 <junkotahara911@gmail.com>
2021-05-20 15:07:22 -0400

    Add api versioning

--- app/controllers/task_logs_controller.rb
+++
@@ -1,2 +0,0 @@
-class TaskLogsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :task_logs
-  resources :tasks
-  resources :projects
-  resources :customers
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :task_logs
+      resources :tasks
+      resources :projects
+      resources :customers
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
