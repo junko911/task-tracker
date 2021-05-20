e13e493499821d5a1e6188fd13950ade453a2dee
junko911 <junkotahara911@gmail.com>
2021-05-20 15:33:56 -0400

    Add email and password to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_183052) do
+ActiveRecord::Schema.define(version: 2021_05_20_193250) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2021_05_20_183052) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "projects", "customers"
