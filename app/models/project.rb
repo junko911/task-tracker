60b959ddd8e3aca00e5d14327670358a61f3229f
junko911 <junkotahara911@gmail.com>
2021-05-23 20:26:55 -0400

    Add validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :customer
+
+  validates :name, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :description, presence: true
 end
