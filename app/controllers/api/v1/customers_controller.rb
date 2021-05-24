58aae7a80ebad6fe8a65b6d3441365bb972184a0
junko911 <junkotahara911@gmail.com>
2021-05-24 14:13:18 -0400

    Add Customer index/show/update/destroy actions

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::CustomersController < ApplicationController
+  before_action :find_customer, only: [:show, :update, :destroy]
+
+  def index
+    render json: Customer.all
+  end
+
+  def show
+    render json: @customer
+  end
+
   def create
     @customer = Customer.new(customer_params)
 
@@ -9,9 +19,22 @@ class Api::V1::CustomersController < ApplicationController
     end
   end
 
+  def update
+    @customer.update(:customer_params)
+    render json: @customer
+  end
+
+  def destroy
+    @customer.destroy
+  end
+
   private
 
   def customer_params
     params.require(:customer).permit(:name)
+  end
+
+  def find_customer
+    @customer = Customer.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
       resources :users, only: [:create]
       post "/login", to: "auth#create"
       get "/profile", to: "users#profile"
+      resources :customers, only: %i(index show create update destroy)
       resources :task_logs
       resources :tasks
       resources :projects
-      resources :customers
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
