41c24d0651ce744faa9d34a2b9e89ec5bfe07226
junko911 <junkotahara911@gmail.com>
2021-05-23 20:28:13 -0400

    Add Customer create action

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::CustomersController < ApplicationController
+  def create
+    @customer = Customer.create(customer_params)
+
+    if @customer.valid?
+      render json: @customer
+    else
+      render json: @customer.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit!
+  end
 end
