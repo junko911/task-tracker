d3f25f29daf8537a63f05221a6dddd69112d9a93
junko911 <junkotahara911@gmail.com>
2021-05-25 10:11:41 -0400

    Modify task_logs table to log start/end time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_193250) do
+ActiveRecord::Schema.define(version: 2021_05_25_140047) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -27,11 +27,12 @@ ActiveRecord::Schema.define(version: 2021_05_20_193250) do
   end
 
   create_table "task_logs", force: :cascade do |t|
-    t.integer "duration_minutes"
     t.integer "task_id", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.index ["task_id"], name: "index_task_logs_on_task_id"
     t.index ["user_id"], name: "index_task_logs_on_user_id"
   end
